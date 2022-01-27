5378fcbf69e0e936f0274a6b646faaf450aa1c21
jb-c <jacob.ox@outlook.com>
2022-01-27 12:31:25 +0000

    Begun exploring the MIMC3 dataset

--- data/loader.py
+++ data/loader.py
@@ -1,3 +1,1 @@
 import numpy as np
-a = np.array([2])
-#code edited remotely
---
+++ data/path.py
@@ -0,0 +1,25 @@
+import pandas as pd
+# The path to the MIMIC 3 Data folders, as formatted by https://github.com/YerevaNN/mimic3-benchmarks
+# The root dir for all of the FORMATTED MIMIC3-data
+root = '../../mimic3-benchmarks/data/'
+root = '/mnt/sdd/MSc_projects/jbarker/code//mimic3-benchmarks/data/'
+
+# A list of sub folders, one for each of the tasks commonly performed on the MIMIC3 dataset
+task_names = ['in-hospital-mortality','length-of-stay','phenotyping']
+selected_task_index = 0
+
+# Task specific paths
+task_root_path = f'{root}/{task_names[selected_task_index]}'
+train_path = f'{task_root_path}/train'
+test_path = f'{task_root_path}/test'
+
+# Lists of individual episode file paths
+# Ensures train/test_episode_paths are in same order as listfile
+train_listfile_path = f'{train_path}/listfile.csv'
+test_listfile_path  = f'{test_path}/listfile.csv'
+
+train_listfile = pd.read_csv(train_listfile_path)
+test_listfile  = pd.read_csv(test_listfile_path)
+
+train_episode_paths = [f'{train_path}/{f}' for f in train_listfile.stay.values]
+test_episode_paths  = [f'{test_path}/{f}' for f in test_listfile.stay.values]
